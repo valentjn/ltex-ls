e55f8b7e7b2694020f544cde816c48ca59f3f349
Adam Voss <vossad01@gmail.com>
2017-06-03 13:57:10 -0500

    Add Travis CI build

--- src/test/java/DocumentPositionCalculatorTest.java
+++ src/test/java/DocumentPositionCalculatorTest.java
@@ -5,41 +5,41 @@ import org.junit.jupiter.api.Test;
 public class DocumentPositionCalculatorTest {
 
     public final DocumentPositionCalculator sut = new DocumentPositionCalculator("Hello\n" +
-                                           "Enthusiastic\r\n" +
-                                           "Reader!");
+            "Enthusiastic\r\n" +
+            "Reader!");
 
     @Test
-    public void testGetPosition_first_character(){
+    public void testGetPosition_first_character() {
         Position position = sut.getPosition(0);
 
-        Assertions.assertEquals(new Position(0, 0),position);
+        Assertions.assertEquals(new Position(0, 0), position);
     }
 
     @Test
-    public void testGetPosition_starts_with_newline(){
-        Position position = sut.getPosition(1);
+    public void testGetPosition_starts_with_newline() {
+        Position position = new DocumentPositionCalculator("\nHi").getPosition(1);
 
-        Assertions.assertEquals(new Position(1, 0),position);
+        Assertions.assertEquals(new Position(1, 0), position);
     }
 
     @Test
-    public void testGetPosition_second_start(){
+    public void testGetPosition_second_line_start() {
         Position position = sut.getPosition(6);
 
-        Assertions.assertEquals(new Position(1, 0),position);
+        Assertions.assertEquals(new Position(1, 0), position);
     }
 
     @Test
-    public void testGetPosition_second_line_second_character(){
+    public void testGetPosition_second_line_second_character() {
         Position position = sut.getPosition(7);
 
-        Assertions.assertEquals(new Position(1, 1),position);
+        Assertions.assertEquals(new Position(1, 1), position);
     }
 
     @Test
-    public void testGetPosition(){
+    public void testGetPosition_arbitrary_position() {
         Position position = sut.getPosition(12);
 
-        Assertions.assertEquals(new Position(1, 6),position);
+        Assertions.assertEquals(new Position(1, 6), position);
     }
 }
