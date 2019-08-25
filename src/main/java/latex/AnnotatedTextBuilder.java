7f51161da36544a3dab58ecb02bedf083a5a733f
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-08-25 20:08:23 +0200

    Improve handling of spaces and punct., refactor

--- src/main/java/latex/AnnotatedTextBuilder.java
+++ src/main/java/latex/AnnotatedTextBuilder.java
@@ -18,15 +18,15 @@ public class AnnotatedTextBuilder {
   private String text;
   private int pos;
   private int pseudoCounter;
-  private String lastPunctuation;
   private String lastSpace;
+  private String dummyLastSpace;
+  private String dummyLastPunctuation;
+  private boolean preserveDummyLast;
   private Stack<Mode> modeStack;
 
   private char curChar;
   private String curString;
   private Mode curMode;
-  private boolean keepLastPunctuation;
-  private boolean keepLastSpace;
 
   private String matchFromPosition(Pattern pattern) {
     Matcher matcher = pattern.matcher(text.substring(pos));
@@ -39,10 +39,49 @@ public class AnnotatedTextBuilder {
     if (curMode == Mode.TEXT) {
       dummy = "Abc" + (pseudoCounter++);
     } else {
-      dummy = "Abc" + (pseudoCounter++) + lastPunctuation;
+      dummy = "Abc" + (pseudoCounter++) + dummyLastPunctuation + dummyLastSpace;
     }
 
+    dummyLastSpace = "";
+    dummyLastPunctuation = "";
     return dummy;
+  }
+
+  private void addText(String text) {
+    if (text.isEmpty()) return;
+    builder.addText(text);
+    pos += text.length();
+    textAdded(text);
+  }
+
+  private void addMarkup(String markup) {
+    if (markup.isEmpty()) return;
+    builder.addMarkup(markup);
+    pos += markup.length();
+
+    if (preserveDummyLast) {
+      preserveDummyLast = false;
+    } else {
+      dummyLastSpace = "";
+      dummyLastPunctuation = "";
+    }
+  }
+
+  private void addMarkup(String markup, String interpretAs) {
+    if (interpretAs.isEmpty()) {
+      addMarkup(markup);
+    } else {
+      builder.addMarkup(markup, interpretAs);
+      pos += markup.length();
+      preserveDummyLast = false;
+      textAdded(interpretAs);
+    }
+  }
+
+  private void textAdded(String text) {
+    if (text.isEmpty()) return;
+    char lastChar = text.charAt(text.length() - 1);
+    lastSpace = ((lastChar == ' ') ? " " : "");
   }
 
   public void addCode(String text) {
@@ -59,27 +98,30 @@ public class AnnotatedTextBuilder {
     this.text = text;
     pos = 0;
     pseudoCounter = 0;
-    lastPunctuation = "";
     lastSpace = "";
+    dummyLastSpace = "";
+    dummyLastPunctuation = "";
+    preserveDummyLast = false;
 
     modeStack = new Stack<Mode>();
     modeStack.push(Mode.TEXT);
+
+    boolean canInsertSpaceBeforeDummy = false;
+    boolean preserveCanInsertSpaceBeforeDummy = false;
 
     while (pos < text.length()) {
       curChar = text.charAt(pos);
       curString = String.valueOf(curChar);
       curMode = modeStack.peek();
-      keepLastPunctuation = false;
-      keepLastSpace = false;
+      preserveCanInsertSpaceBeforeDummy = false;
 
       switch (curChar) {
         case '\\': {
           String command = matchFromPosition(commandPattern);
 
           if (command.equals("\\begin") || command.equals("\\end")) {
-            builder.addMarkup(command);
-            keepLastPunctuation = true;
-            pos += command.length();
+            preserveDummyLast = true;
+            addMarkup(command);
 
             String argument = matchFromPosition(argumentPattern);
             String environment = argument.substring(1, argument.length() - 1);
@@ -88,11 +130,12 @@ public class AnnotatedTextBuilder {
             if (Arrays.asList(mathEnvironments).contains(environment)) {
               if (command.equals("\\begin")) {
                 modeStack.push(Mode.MATH);
+                canInsertSpaceBeforeDummy = true;
+                preserveCanInsertSpaceBeforeDummy = true;
               } else {
                 modeStack.pop();
                 if (modeStack.isEmpty()) modeStack.push(Mode.TEXT);
                 interpretAs = constructDummy();
-                keepLastPunctuation = false;
               }
             } else {
               if (command.equals("\\begin")) {
@@ -103,83 +146,71 @@ public class AnnotatedTextBuilder {
               }
             }
 
-            builder.addMarkup(argument, interpretAs);
-            keepLastSpace = interpretAs.isEmpty();
-            pos += argument.length();
+            preserveDummyLast = true;
+            addMarkup(argument, interpretAs);
           } else if (command.equals("\\$") || command.equals("\\%") || command.equals("\\&")) {
-            builder.addMarkup(command, command.substring(1));
-            pos += command.length();
+            addMarkup(command, command.substring(1));
+          } else if (command.equals("\\,") || command.equals("\\;") || command.equals("\\quad")) {
+            if ((curMode == Mode.MATH) && (canInsertSpaceBeforeDummy)) {
+              addMarkup(command, " ");
+            } else {
+              preserveDummyLast = true;
+              addMarkup(command);
+              dummyLastSpace = " ";
+            }
           } else if (command.equals("\\cite") || command.equals("\\cref") ||
               command.equals("\\Cref") || command.equals("\\includegraphics") ||
               command.equals("\\ref")) {
-            builder.addMarkup(command, constructDummy());
-            pos += command.length();
-
-            String optionalArgument = matchFromPosition(optionalArgumentPattern);
-            builder.addMarkup(optionalArgument);
-            pos += optionalArgument.length();
-
-            String argument = matchFromPosition(argumentPattern);
-            builder.addMarkup(argument);
-            pos += argument.length();
-
-            keepLastSpace = true;
+            addMarkup(command, constructDummy());
+            addMarkup(matchFromPosition(optionalArgumentPattern));
+            addMarkup(matchFromPosition(argumentPattern));
           } else if (command.equals("\\footnote")) {
             if (lastSpace.isEmpty()) {
-              builder.addMarkup(command, " ");
+              addMarkup(command, " ");
               lastSpace = " ";
             } else {
-              builder.addMarkup(command);
+              addMarkup(command);
             }
-
-            keepLastSpace = true;
-            pos += command.length();
           } else if (command.equals("\\text")) {
             modeStack.push(Mode.TEXT);
             String interpretAs = ((curMode == Mode.MATH) ? constructDummy() : "");
-            builder.addMarkup(command + "{", interpretAs);
-            keepLastSpace = interpretAs.isEmpty();
-            pos += command.length() + 1;
+            addMarkup(command + "{", interpretAs);
           } else {
-            builder.addMarkup(command);
-            keepLastSpace = true;
-            pos += command.length();
+            addMarkup(command);
           }
 
           break;
         }
         case '{': {
           modeStack.push(curMode);
-          builder.addMarkup(curString);
-          keepLastSpace = true;
-          pos++;
+          addMarkup(curString);
           break;
         }
         case '}': {
           modeStack.pop();
-          builder.addMarkup(curString);
-          keepLastSpace = true;
-          pos++;
+          addMarkup(curString);
+          canInsertSpaceBeforeDummy = true;
+          preserveCanInsertSpaceBeforeDummy = true;
           break;
         }
         case '$': {
           if (curMode == Mode.TEXT) {
             modeStack.push(Mode.MATH);
-            builder.addMarkup(curString);
-            keepLastSpace = true;
+            addMarkup(curString);
+            canInsertSpaceBeforeDummy = true;
+            preserveCanInsertSpaceBeforeDummy = true;
           } else {
             modeStack.pop();
-            builder.addMarkup(curString, constructDummy());
+            addMarkup(curString, constructDummy());
           }
 
-          pos++;
           break;
         }
         case '%': {
           String comment = matchFromPosition(commentPattern);
-          builder.addMarkup(comment);
-          keepLastSpace = true;
-          pos += comment.length();
+          preserveDummyLast = true;
+          preserveCanInsertSpaceBeforeDummy = true;
+          addMarkup(comment);
           break;
         }
         case ' ':
@@ -187,22 +218,20 @@ public class AnnotatedTextBuilder {
         case '\r':
         case '\t': {
           String whiteSpace = matchFromPosition(whiteSpacePattern);
+          preserveDummyLast = true;
+          preserveCanInsertSpaceBeforeDummy = true;
 
           if (curMode == Mode.TEXT) {
             if (lastSpace.isEmpty()) {
-              builder.addMarkup(whiteSpace, " ");
+              addMarkup(whiteSpace, " ");
               lastSpace = " ";
             } else {
-              builder.addMarkup(whiteSpace);
+              addMarkup(whiteSpace);
             }
-
-            keepLastSpace = true;
           } else {
-            builder.addMarkup(whiteSpace);
-            keepLastPunctuation = true;
+            addMarkup(whiteSpace);
           }
 
-          pos += whiteSpace.length();
           break;
         }
         case '`':
@@ -226,42 +255,31 @@ public class AnnotatedTextBuilder {
               }
             }
 
-            if (quote.isEmpty()) {
-              builder.addText(curString);
-              pos++;
-            } else {
-              builder.addMarkup(quote, smartQuote);
-              pos += 2;
-            }
+            if (quote.isEmpty()) addText(curString);
+            else addMarkup(quote, smartQuote);
           } else {
-            builder.addMarkup(curString);
-            keepLastSpace = true;
-            pos++;
+            addMarkup(curString);
           }
 
           break;
         }
         default: {
           if (curMode == Mode.TEXT) {
-            builder.addText(curString);
+            addText(curString);
           } else {
+            addMarkup(curString);
+
             if ((curChar == '.') || (curChar == ',') || (curChar == ':') ||
                 (curChar == ';')) {
-              lastPunctuation = curString;
-              keepLastPunctuation = true;
+              dummyLastPunctuation = curString;
             }
-
-            builder.addMarkup(curString);
-            keepLastSpace = true;
           }
 
-          pos++;
           break;
         }
       }
 
-      if (!keepLastPunctuation) lastPunctuation = "";
-      if (!keepLastSpace) lastSpace = "";
+      if (!preserveCanInsertSpaceBeforeDummy) canInsertSpaceBeforeDummy = false;
     }
   }
 
