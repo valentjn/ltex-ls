22f326a2c8cc4a8fa3baa2825d3097e862bfa270
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-08-26 17:19:36 +0200

    Fix wrong argument type matching

--- src/main/java/latex/CommandSignature.java
+++ src/main/java/latex/CommandSignature.java
@@ -32,16 +32,15 @@ public class CommandSignature {
     Pattern parenthesisArgumentPattern = Pattern.compile("^\\([^\\)]*?\\)");
 
     int startPos = pos;
-    int nextArgumentIndex = 0;
     String match = matchFromPosition(text, pos, commandPattern);
     pos += match.length();
 
-    for (int i = 0; i < argumentTypes.size(); i++) {
+    for (ArgumentType argumentType : argumentTypes) {
       match = matchFromPosition(text, pos, commentPattern);
       pos += match.length();
       Pattern argumentPattern = null;
 
-      switch (argumentTypes.get(nextArgumentIndex)) {
+      switch (argumentType) {
         case BRACE: {
           argumentPattern = braceArgumentPattern;
           break;
