20baf6e5a89ec08cccae2750c77f0c759594cac7
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-08-30 17:37:00 +0200

    Ignore first argument of \raisebox and \textcolor

--- src/test/java/latex/AnnotatedTextBuilderTest.java
+++ src/test/java/latex/AnnotatedTextBuilderTest.java
@@ -30,6 +30,8 @@ public class AnnotatedTextBuilderTest {
         "\n" +
         "This is another sentence.\n",
         "This is a sentence.\n\nThis is another sentence. ");
+    assertPlainText("This is a \\textcolor{mittelblau}{test}.\n", "This is a test. ");
+    assertPlainText("This is a \\raisebox{-0.5\\height-0.5mm}{test}.\n", "This is a test. ");
   }
 
   @Test
