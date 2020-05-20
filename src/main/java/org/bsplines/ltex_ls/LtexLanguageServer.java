4c5f0dff3844c8e9eb81502630486c422ee5071a
Julian Valentin <julian.valentin@bsplines.org>
2020-05-20 21:32:36 +0200

    Send progress events during validateDocument

--- src/main/java/org/bsplines/ltex_ls/LtexLanguageServer.java
+++ src/main/java/org/bsplines/ltex_ls/LtexLanguageServer.java
@@ -431,151 +431,166 @@ public class LtexLanguageServer implements LanguageServer, LanguageClientAware {
         });
   }
 
+  private void sendProgressEvent(String uri, String operation, double progress) {
+    // real progress events from LSP 3.15 are not implemented yet in LSP4J
+    // (see https://github.com/eclipse/lsp4j/issues/370)
+    JsonObject arguments = new JsonObject();
+    arguments.addProperty("type", "progress");
+    arguments.addProperty("uri", uri);
+    arguments.addProperty("operation", operation);
+    arguments.addProperty("progress", progress);
+    client.telemetryEvent(arguments);
+  }
+
   private CompletableFuture<Pair<List<LanguageToolRuleMatch>, AnnotatedText>> validateDocument(
-      TextDocumentItem document) {
+        TextDocumentItem document) {
     ConfigurationItem configurationItem = new ConfigurationItem();
     configurationItem.setSection("ltex");
     configurationItem.setScopeUri(document.getUri());
     CompletableFuture<List<Object>> configurationFuture = client.configuration(
         new ConfigurationParams(Arrays.asList(configurationItem)));
+    sendProgressEvent(document.getUri(), "validateDocument", 0);
 
-    return configurationFuture.thenApply(
-        (List<Object> configuration) -> {
-          setSettings((JsonElement) configuration.get(0));
+    return configurationFuture.thenApply((List<Object> configuration) -> {
+      try {
+        setSettings((JsonElement) configuration.get(0));
 
-          if (languageToolInterface == null) {
-            Tools.logger.warning(Tools.i18n("skippingTextCheck"));
-            return new Pair<>(Collections.emptyList(), null);
+        if (languageToolInterface == null) {
+          Tools.logger.warning(Tools.i18n("skippingTextCheck"));
+          return new Pair<>(Collections.emptyList(), null);
+        }
+
+        String codeLanguageId = document.getLanguageId();
+        AnnotatedText annotatedText;
+
+        switch (codeLanguageId) {
+          case "plaintext": {
+            AnnotatedTextBuilder builder = new AnnotatedTextBuilder();
+            annotatedText = builder.addText(document.getText()).build();
+            break;
           }
+          case "markdown": {
+            Parser p = Parser.builder().build();
+            Document mdDocument = p.parse(document.getText());
 
-          String codeLanguageId = document.getLanguageId();
-          AnnotatedText annotatedText;
+            MarkdownAnnotatedTextBuilder builder = new MarkdownAnnotatedTextBuilder();
+            builder.language = settings.getLanguageShortCode();
+            builder.dummyNodeTypes.addAll(settings.getDummyMarkdownNodeTypes());
+            builder.ignoreNodeTypes.addAll(settings.getIgnoreMarkdownNodeTypes());
 
-          switch (codeLanguageId) {
-            case "plaintext": {
-              AnnotatedTextBuilder builder = new AnnotatedTextBuilder();
-              annotatedText = builder.addText(document.getText()).build();
-              break;
+            builder.visit(mdDocument);
+
+            annotatedText = builder.getAnnotatedText();
+            break;
+          }
+          case "latex":
+          case "rsweave": {
+            LatexAnnotatedTextBuilder builder = new LatexAnnotatedTextBuilder();
+            builder.language = settings.getLanguageShortCode();
+            builder.codeLanguageId = codeLanguageId;
+
+            for (String commandPrototype : settings.getDummyCommandPrototypes()) {
+              builder.commandSignatures.add(new LatexCommandSignature(commandPrototype,
+                  LatexCommandSignature.Action.DUMMY));
             }
-            case "markdown": {
-              Parser p = Parser.builder().build();
-              Document mdDocument = p.parse(document.getText());
 
-              MarkdownAnnotatedTextBuilder builder = new MarkdownAnnotatedTextBuilder();
-              builder.language = settings.getLanguageShortCode();
-              builder.dummyNodeTypes.addAll(settings.getDummyMarkdownNodeTypes());
-              builder.ignoreNodeTypes.addAll(settings.getIgnoreMarkdownNodeTypes());
-
-              builder.visit(mdDocument);
-
-              annotatedText = builder.getAnnotatedText();
-              break;
+            for (String commandPrototype : settings.getIgnoreCommandPrototypes()) {
+              builder.commandSignatures.add(new LatexCommandSignature(commandPrototype,
+                  LatexCommandSignature.Action.IGNORE));
             }
-            case "latex":
-            case "rsweave": {
-              LatexAnnotatedTextBuilder builder = new LatexAnnotatedTextBuilder();
-              builder.language = settings.getLanguageShortCode();
-              builder.codeLanguageId = codeLanguageId;
 
-              for (String commandPrototype : settings.getDummyCommandPrototypes()) {
-                builder.commandSignatures.add(new LatexCommandSignature(commandPrototype,
-                    LatexCommandSignature.Action.DUMMY));
+            builder.ignoreEnvironments.addAll(settings.getIgnoreEnvironments());
+
+            ExecutorService executor = Executors.newCachedThreadPool();
+            Future<Object> builderFuture = executor.submit(new Callable<Object>() {
+              public Object call() throws InterruptedException {
+                builder.addCode(document.getText());
+                return null;
               }
+            });
 
-              for (String commandPrototype : settings.getIgnoreCommandPrototypes()) {
-                builder.commandSignatures.add(new LatexCommandSignature(commandPrototype,
-                    LatexCommandSignature.Action.IGNORE));
-              }
-
-              builder.ignoreEnvironments.addAll(settings.getIgnoreEnvironments());
-
-              ExecutorService executor = Executors.newCachedThreadPool();
-              Future<Object> builderFuture = executor.submit(new Callable<Object>() {
-                public Object call() throws InterruptedException {
-                  builder.addCode(document.getText());
-                  return null;
-                }
-              });
-
-              try {
-                builderFuture.get(10, TimeUnit.SECONDS);
-              } catch (TimeoutException | InterruptedException | ExecutionException e) {
-                throw new RuntimeException(Tools.i18n("latexAnnotatedTextBuilderFailed"), e);
-              } finally {
-                builderFuture.cancel(true);
-              }
-
-              annotatedText = builder.getAnnotatedText();
-              break;
-            }
-            default: {
-              throw new UnsupportedOperationException(Tools.i18n(
-                  "codeLanguageNotSupported", codeLanguageId));
-            }
-          }
-
-          if ((settings.getDictionary().size() >= 1) &&
-              "BsPlInEs".equals(settings.getDictionary().get(0))) {
-            languageToolInterface.enableEasterEgg();
-          }
-
-          {
-            int logTextMaxLength = 100;
-            String logText = annotatedText.getPlainText();
-            String postfix = "";
-
-            if (logText.length() > logTextMaxLength) {
-              logText = logText.substring(0, logTextMaxLength);
-              postfix = Tools.i18n("truncatedPostfix", logTextMaxLength);
+            try {
+              builderFuture.get(10, TimeUnit.SECONDS);
+            } catch (TimeoutException | InterruptedException | ExecutionException e) {
+              throw new RuntimeException(Tools.i18n("latexAnnotatedTextBuilderFailed"), e);
+            } finally {
+              builderFuture.cancel(true);
             }
 
-            Tools.logger.info(Tools.i18n("checkingText",
-                settings.getLanguageShortCode(), StringEscapeUtils.escapeJava(logText), postfix));
+            annotatedText = builder.getAnnotatedText();
+            break;
+          }
+          default: {
+            throw new UnsupportedOperationException(Tools.i18n(
+                "codeLanguageNotSupported", codeLanguageId));
+          }
+        }
+
+        if ((settings.getDictionary().size() >= 1) &&
+            "BsPlInEs".equals(settings.getDictionary().get(0))) {
+          languageToolInterface.enableEasterEgg();
+        }
+
+        {
+          int logTextMaxLength = 100;
+          String logText = annotatedText.getPlainText();
+          String postfix = "";
+
+          if (logText.length() > logTextMaxLength) {
+            logText = logText.substring(0, logTextMaxLength);
+            postfix = Tools.i18n("truncatedPostfix", logTextMaxLength);
           }
 
-          try {
-            List<LanguageToolRuleMatch> result = languageToolInterface.check(annotatedText);
+          Tools.logger.info(Tools.i18n("checkingText",
+              settings.getLanguageShortCode(), StringEscapeUtils.escapeJava(logText), postfix));
+        }
 
-            Tools.logger.info((result.size() == 1) ? Tools.i18n("obtainedRuleMatch") :
-                Tools.i18n("obtainedRuleMatches", result.size()));
+        try {
+          List<LanguageToolRuleMatch> result = languageToolInterface.check(annotatedText);
 
-            List<IgnoreRuleSentencePair> ignoreRuleSentencePairs =
-                settings.getIgnoreRuleSentencePairs();
+          Tools.logger.info((result.size() == 1) ? Tools.i18n("obtainedRuleMatch") :
+              Tools.i18n("obtainedRuleMatches", result.size()));
 
-            if (!result.isEmpty() && !ignoreRuleSentencePairs.isEmpty()) {
-              List<LanguageToolRuleMatch> ignoreMatches = new ArrayList<>();
+          List<IgnoreRuleSentencePair> ignoreRuleSentencePairs =
+              settings.getIgnoreRuleSentencePairs();
 
-              for (LanguageToolRuleMatch match : result) {
-                if (match.getSentence() != null) {
-                  String ruleId = match.getRuleId();
-                  String sentence = match.getSentence().trim();
+          if (!result.isEmpty() && !ignoreRuleSentencePairs.isEmpty()) {
+            List<LanguageToolRuleMatch> ignoreMatches = new ArrayList<>();
 
-                  for (IgnoreRuleSentencePair pair : ignoreRuleSentencePairs) {
-                    if (pair.getRuleId().equals(ruleId) &&
-                          pair.getSentencePattern().matcher(sentence).find()) {
-                      Tools.logger.info(Tools.i18n("removingIgnoredRuleMatch", ruleId, sentence));
-                      ignoreMatches.add(match);
-                      break;
-                    }
+            for (LanguageToolRuleMatch match : result) {
+              if (match.getSentence() != null) {
+                String ruleId = match.getRuleId();
+                String sentence = match.getSentence().trim();
+
+                for (IgnoreRuleSentencePair pair : ignoreRuleSentencePairs) {
+                  if (pair.getRuleId().equals(ruleId) &&
+                        pair.getSentencePattern().matcher(sentence).find()) {
+                    Tools.logger.info(Tools.i18n("removingIgnoredRuleMatch", ruleId, sentence));
+                    ignoreMatches.add(match);
+                    break;
                   }
                 }
               }
-
-              if (!ignoreMatches.isEmpty()) {
-                Tools.logger.info((ignoreMatches.size() == 1) ?
-                    Tools.i18n("removedIgnoredRuleMatch") :
-                    Tools.i18n("removedIgnoredRuleMatches", ignoreMatches.size()));
-                for (LanguageToolRuleMatch match : ignoreMatches) result.remove(match);
-              }
             }
 
-            return new Pair<>(result, annotatedText);
-          } catch (RuntimeException e) {
-            Tools.logger.severe(Tools.i18n("languageToolFailed", e.getMessage()));
-            e.printStackTrace();
-            return new Pair<>(Collections.emptyList(), annotatedText);
+            if (!ignoreMatches.isEmpty()) {
+              Tools.logger.info((ignoreMatches.size() == 1) ?
+                  Tools.i18n("removedIgnoredRuleMatch") :
+                  Tools.i18n("removedIgnoredRuleMatches", ignoreMatches.size()));
+              for (LanguageToolRuleMatch match : ignoreMatches) result.remove(match);
+            }
           }
-        });
+
+          return new Pair<>(result, annotatedText);
+        } catch (RuntimeException e) {
+          Tools.logger.severe(Tools.i18n("languageToolFailed", e.getMessage()));
+          e.printStackTrace();
+          return new Pair<>(Collections.emptyList(), annotatedText);
+        }
+      } finally {
+        sendProgressEvent(document.getUri(), "validateDocument", 1);
+      }
+    });
   }
 
   @Override
