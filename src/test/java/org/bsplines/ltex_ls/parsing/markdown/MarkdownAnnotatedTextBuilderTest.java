528d05d064f2b594fb385b317dafc94d206a9542
Julian Valentin <julian.valentin@bsplines.org>
2020-05-24 15:46:56 +0200

    Test ignore/dummy blocks for Markdown

--- src/test/java/org/bsplines/ltex_ls/parsing/markdown/MarkdownAnnotatedTextBuilderTest.java
+++ src/test/java/org/bsplines/ltex_ls/parsing/markdown/MarkdownAnnotatedTextBuilderTest.java
@@ -1,21 +1,46 @@
 package org.bsplines.ltex_ls.parsing.markdown;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import org.languagetool.markup.AnnotatedText;
 
-import java.io.IOException;
-
 public class MarkdownAnnotatedTextBuilderTest {
-  @Test
-  void test() throws IOException {
-    MarkdownAnnotatedTextBuilder builder = new MarkdownAnnotatedTextBuilder();
-    builder.addCode("# Heading\n" +
-        "Paragraph with\n" +
-        "multiple lines and [link](example.com)");
-    AnnotatedText text = builder.build();
+  private static void assertPlainText(String code, String expectedPlainText) {
+    assertPlainText(code, expectedPlainText, Collections.emptyList(), Collections.emptyList());
+  }
 
-    Assertions.assertEquals("Heading\nParagraph with multiple lines and link", text.getPlainText());
+  private static void assertPlainText(String code, String expectedPlainText,
+    List<String> ignoreNodeTypes, List<String> dummyNodeTypes) {
+    AnnotatedText annotatedText = buildAnnotatedText(code, ignoreNodeTypes, dummyNodeTypes);
+    Assertions.assertEquals(expectedPlainText, annotatedText.getPlainText());
+  }
+
+  private static AnnotatedText buildAnnotatedText(String code,
+        List<String> ignoreNodeTypes, List<String> dummyNodeTypes) {
+    MarkdownAnnotatedTextBuilder builder = new MarkdownAnnotatedTextBuilder();
+    builder.ignoreNodeTypes.addAll(ignoreNodeTypes);
+    builder.dummyNodeTypes.addAll(dummyNodeTypes);
+    return builder.addCode(code).build();
+  }
+
+  @Test
+  public void test() throws IOException {
+    assertPlainText(
+        "# Heading\n" +
+        "Paragraph with\n" +
+        "multiple lines and [link](example.com)\n",
+        "Heading\nParagraph with multiple lines and link\n");
+    assertPlainText(
+        "This is a test: `inline code`.\n\n```\ncode block\n```\n\nThis is another sentence.\n",
+        "This is a test: inline code.\n\n\ncode block\n\n\nThis is another sentence.\n");
+    assertPlainText(
+        "This is a test: `inline code`.\n\n```\ncode block\n```\n\nThis is another sentence.\n",
+        "This is a test: Dummy0.\n\n\n\n\n\nThis is another sentence.\n",
+        Collections.singletonList("FencedCodeBlock"), Collections.singletonList("Code"));
   }
 }
