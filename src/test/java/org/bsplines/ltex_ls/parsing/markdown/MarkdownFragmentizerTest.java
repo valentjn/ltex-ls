9fad04796405d2ef6f0488ffd311785a2aab5a1f
Julian Valentin <julian.valentin@bsplines.org>
2020-05-24 15:59:21 +0200

    Add MarkdownFragmentizerTest

---
+++ src/test/java/org/bsplines/ltex_ls/parsing/markdown/MarkdownFragmentizerTest.java
@@ -0,0 +1,40 @@
+package org.bsplines.ltex_ls.parsing.markdown;
+
+import java.util.List;
+
+import org.bsplines.ltex_ls.Settings;
+import org.bsplines.ltex_ls.parsing.CodeFragment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MarkdownFragmentizerTest {
+  @Test
+  public void test() {
+    MarkdownFragmentizer markdownFragmentizer =
+        new MarkdownFragmentizer("markdown", new Settings());
+    List<CodeFragment> codeFragments = markdownFragmentizer.fragmentize(
+        "Sentence 1\n" +
+        "\n[comment]: # ltex: language=de-DE\n\nSentence 2\n" +
+        "\n[comment]:\t#ltex:\tlanguage=en-US\n\nSentence 3\n");
+    Assertions.assertEquals(3, codeFragments.size());
+
+    Assertions.assertEquals("markdown", codeFragments.get(0).getCodeLanguageId());
+    Assertions.assertEquals("Sentence 1\n\n[comment]: ",
+        codeFragments.get(0).getCode());
+    Assertions.assertEquals(0, codeFragments.get(0).getFromPos());
+    Assertions.assertEquals("en-US", codeFragments.get(0).getSettings().getLanguageShortCode());
+
+    Assertions.assertEquals("markdown", codeFragments.get(1).getCodeLanguageId());
+    Assertions.assertEquals("# ltex: language=de-DE\n\nSentence 2\n\n[comment]:\t",
+        codeFragments.get(1).getCode());
+    Assertions.assertEquals(23, codeFragments.get(1).getFromPos());
+    Assertions.assertEquals("de-DE", codeFragments.get(1).getSettings().getLanguageShortCode());
+
+    Assertions.assertEquals("markdown", codeFragments.get(2).getCodeLanguageId());
+    Assertions.assertEquals("#ltex:\tlanguage=en-US\n\nSentence 3\n",
+        codeFragments.get(2).getCode());
+    Assertions.assertEquals(70, codeFragments.get(2).getFromPos());
+    Assertions.assertEquals("en-US", codeFragments.get(2).getSettings().getLanguageShortCode());
+  }
+}
