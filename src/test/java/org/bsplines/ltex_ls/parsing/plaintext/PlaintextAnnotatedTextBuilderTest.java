c23dd02c6df5cb4921db4d76b73f1dba95e94a1f
Julian Valentin <julian.valentin@bsplines.org>
2020-05-26 19:42:17 +0200

    Add test for CodeAnnotatedTextBuilder

--- src/test/java/org/bsplines/ltex_ls/parsing/plaintext/PlaintextAnnotatedTextBuilderTest.java
+++ src/test/java/org/bsplines/ltex_ls/parsing/plaintext/PlaintextAnnotatedTextBuilderTest.java
@@ -1,5 +1,6 @@
 package org.bsplines.ltex_ls.parsing.plaintext;
 
+import org.bsplines.ltex_ls.Settings;
 import org.bsplines.ltex_ls.parsing.CodeAnnotatedTextBuilder;
 
 import org.junit.jupiter.api.Assertions;
@@ -14,5 +15,6 @@ public class PlaintextAnnotatedTextBuilderTest {
     builder.addCode("This is \\textbf{a} `test`.\n");
     AnnotatedText annotatedText = builder.build();
     Assertions.assertEquals("This is \\textbf{a} `test`.\n", annotatedText.getPlainText());
+    Assertions.assertDoesNotThrow(() -> builder.setSettings(new Settings()));
   }
 }
