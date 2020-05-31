9827d54a76e47a33f309632dff65eab59c83027f
Julian Valentin <julian.valentin@bsplines.org>
2020-05-31 09:59:09 +0200

    Add possibility for plural dummies

--- src/main/java/org/bsplines/ltex_ls/parsing/DummyGenerator.java
+++ src/main/java/org/bsplines/ltex_ls/parsing/DummyGenerator.java
@@ -1,20 +1,34 @@
 package org.bsplines.ltex_ls.parsing;
 
 public class DummyGenerator {
-  private static DummyGenerator defaultDummyGenerator = new DummyGenerator();
+  private boolean isPlural_;
+
+  private static DummyGenerator default_ = new DummyGenerator();
+  private static DummyGenerator defaultPlural = new DummyGenerator(true);
 
   public DummyGenerator() {
+    this(false);
+  }
+
+  public DummyGenerator(boolean isPlural_) {
+    this.isPlural_ = isPlural_;
   }
 
   public static DummyGenerator getDefault() {
-    return defaultDummyGenerator;
+    return getDefault(false);
+  }
+
+  public static DummyGenerator getDefault(boolean isPlural_) {
+    return (isPlural_ ? defaultPlural : default_);
   }
 
   public String generate(String language, int number) {
     if (language.equalsIgnoreCase("fr")) {
       return "Jimmy-" + number;
     } else {
-      return "Dummy" + number;
+      return (isPlural_ ? "Dummies" : ("Dummy" + number));
     }
   }
+
+  public boolean isPlural() { return isPlural_; }
 }
