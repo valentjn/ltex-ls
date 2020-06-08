0b41a5b2b35e9d2c34490d3185e9a77037643671
Julian Valentin <julian.valentin@bsplines.org>
2020-06-08 18:49:16 +0200

    Fix style of DocumentPositionCalculator

--- src/main/java/org/bsplines/ltexls/DocumentPositionCalculator.java
+++ src/main/java/org/bsplines/ltexls/DocumentPositionCalculator.java
@@ -20,7 +20,7 @@ class DocumentPositionCalculator {
     int line = Arrays.binarySearch(lineStarts, pos);
 
     if (line < 0) {
-      int insertionPoint = -1 * line - 1;
+      int insertionPoint = -line - 1;
       line = insertionPoint - 1;
     }
 
@@ -30,14 +30,12 @@ class DocumentPositionCalculator {
   private static int[] getLineStartPositions(String text) {
     ArrayList<Integer> lineStartPositions = new ArrayList<>();
     lineStartPositions.add(0);
+
     for (int i = 0; i < text.length(); i++) {
       char c = text.charAt(i);
 
       if (c == '\r') {
-        if (i + 1 < text.length() && text.charAt(i + 1) == '\n') {
-          i++;
-        }
-
+        if ((i + 1 < text.length()) && (text.charAt(i + 1) == '\n')) i++;
         lineStartPositions.add(i + 1);
       } else if (c == '\n') {
         lineStartPositions.add(i + 1);
