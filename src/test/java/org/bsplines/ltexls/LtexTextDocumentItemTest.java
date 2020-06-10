292547daa628c6e30612aed483dd9e30ab17d212
Julian Valentin <julian.valentin@bsplines.org>
2020-06-10 18:39:07 +0200

    Add test for inverse conversion of positions

--- src/test/java/org/bsplines/ltexls/LtexTextDocumentItemTest.java
+++ src/test/java/org/bsplines/ltexls/LtexTextDocumentItemTest.java
@@ -6,18 +6,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LtexTextDocumentItemTest {
+  private static void assertPosition(LtexTextDocumentItem document, int pos, Position position) {
+    Assertions.assertEquals(position, document.convertPosition(pos));
+    Assertions.assertEquals(pos, document.convertPosition(position));
+  }
+
   @Test
   public void testConvertPosition() {
     LtexTextDocumentItem document;
 
     document = new LtexTextDocumentItem("untitled:test.txt", "plaintext", 1,
         "Hello\nEnthusiastic\r\nReader!");
-    Assertions.assertEquals(new Position(0, 0), document.convertPosition(0));
-    Assertions.assertEquals(new Position(1, 0), document.convertPosition(6));
-    Assertions.assertEquals(new Position(1, 1), document.convertPosition(7));
-    Assertions.assertEquals(new Position(1, 6), document.convertPosition(12));
+    assertPosition(document, 0, new Position(0, 0));
+    assertPosition(document, 6, new Position(1, 0));
+    assertPosition(document, 7, new Position(1, 1));
+    assertPosition(document, 12, new Position(1, 6));
+    assertPosition(document, 18, new Position(1, 12));
+    assertPosition(document, 20, new Position(2, 0));
 
     document = new LtexTextDocumentItem("untitled:test.txt", "plaintext", 1, "\nHi");
     Assertions.assertEquals(new Position(1, 0), document.convertPosition(1));
+    assertPosition(document, 1, new Position(1, 0));
   }
 }
