66703065dd3a19b02a0362950f4b08647289ac2e
Julian Valentin <julian.valentin@bsplines.org>
2020-06-23 18:33:40 +0200

    Change default of all variables to NonNull

---
+++ ltexls-core/src/main/java/org/bsplines/ltexls/package-info.java
@@ -0,0 +1,5 @@
+@DefaultQualifier(NonNull.class)
+package org.bsplines.ltexls;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
