51ac420eecb707bef47b46a4d2bd8c224e591c93
Julian Valentin <julian.valentin@bsplines.org>
2020-07-26 08:58:15 +0200

    Add test for easter egg

--- ltexls-core/src/test/java/org/bsplines/ltexls/languagetool/LanguageToolJavaInterfaceTest.java
+++ ltexls-core/src/test/java/org/bsplines/ltexls/languagetool/LanguageToolJavaInterfaceTest.java
@@ -7,10 +7,12 @@
 
 package org.bsplines.ltexls.languagetool;
 
+import java.util.Collections;
 import java.util.List;
 import org.bsplines.ltexls.DocumentChecker;
 import org.bsplines.ltexls.DocumentCheckerTest;
 import org.bsplines.ltexls.LtexTextDocumentItem;
+import org.bsplines.ltexls.Settings;
 import org.bsplines.ltexls.SettingsManager;
 import org.bsplines.ltexls.parsing.AnnotatedTextFragment;
 import org.checkerframework.checker.nullness.NullnessUtil;
@@ -31,6 +33,18 @@ public class LanguageToolJavaInterfaceTest {
     Pair<List<LanguageToolRuleMatch>, List<AnnotatedTextFragment>> checkingResult =
         documentChecker.check(document);
     DocumentCheckerTest.assertMatches(checkingResult.getKey(), 8, 10, 58, 75);
+  }
+
+  @Test
+  public void testEasterEgg() {
+    Settings settings = (new Settings()).withDictionary(Collections.singletonList("BsPlInEs"));
+    SettingsManager settingsManager = new SettingsManager(settings);
+    DocumentChecker documentChecker = new DocumentChecker(settingsManager);
+    LtexTextDocumentItem document = DocumentCheckerTest.createDocument("latex",
+        "Hat functions is for a beginner.\n");
+    Pair<List<LanguageToolRuleMatch>, List<AnnotatedTextFragment>> checkingResult =
+        documentChecker.check(document);
+    Assertions.assertEquals(2, checkingResult.getKey().size());
   }
 
   @Test
