21f77a77c68e01f9bcf6fc83395b206b149f186e
Julian Valentin <julian.valentin@bsplines.org>
2021-02-13 20:40:31 +0100

    Add more tests for LtexWorkspaceService

--- ltexls-core/src/test/java/org/bsplines/ltexls/server/LtexWorkspaceServiceTest.java
+++ ltexls-core/src/test/java/org/bsplines/ltexls/server/LtexWorkspaceServiceTest.java
@@ -9,10 +9,10 @@ package org.bsplines.ltexls.server;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 import org.bsplines.ltexls.tools.Tools;
@@ -24,44 +24,94 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LtexWorkspaceServiceTest {
-  @Test
-  public void test() {
-    LtexLanguageServer server = new LtexLanguageServer();
-    LtexWorkspaceService service = new LtexWorkspaceService(server);
-    Assertions.assertDoesNotThrow(() -> service.symbol(new WorkspaceSymbolParams()));
-    JsonObject settings = new JsonObject();
-    settings.add("ltex", new JsonObject());
-    Assertions.assertDoesNotThrow(() -> service.didChangeConfiguration(
-        new DidChangeConfigurationParams(settings)));
-    Assertions.assertDoesNotThrow(() -> service.didChangeWatchedFiles(
-        new DidChangeWatchedFilesParams()));
-  }
-
   private static void assertCheckDocumentResult(String uri, boolean expected)
         throws InterruptedException, ExecutionException {
     LtexLanguageServer server = new LtexLanguageServer();
-    LtexWorkspaceService service = new LtexWorkspaceService(server);
+    final LtexWorkspaceService service = new LtexWorkspaceService(server);
+
+    JsonObject argument = new JsonObject();
+    argument.addProperty("uri", uri);
+
+    final JsonObject range = new JsonObject();
+    JsonObject rangeStart = new JsonObject();
+    rangeStart.addProperty("line", 0);
+    rangeStart.addProperty("character", 1);
+    JsonObject rangeEnd = new JsonObject();
+    rangeEnd.addProperty("line", 2);
+    rangeEnd.addProperty("character", 3);
+    range.add("start", rangeStart);
+    range.add("end", rangeEnd);
+    argument.add("range", range);
+
     ExecuteCommandParams params = new ExecuteCommandParams("ltex.checkDocument",
-        Collections.singletonList(JsonParser.parseString("{\"uri\": \"" + uri + "\"}")));
+        Collections.singletonList(argument));
+
     JsonObject result = ((JsonElement)service.executeCommand(params).get()).getAsJsonObject();
+
     Assertions.assertEquals(false, result.get("success").getAsBoolean());
     Assertions.assertEquals(!expected, result.has("errorMessage"));
   }
 
   @Test
-  public void testExecuteCommand() throws IOException, InterruptedException, ExecutionException,
+  public void testMiscellaneous() throws InterruptedException, ExecutionException {
+    LtexLanguageServer server = new LtexLanguageServer();
+    LtexWorkspaceService service = new LtexWorkspaceService(server);
+    Assertions.assertDoesNotThrow(() -> service.symbol(new WorkspaceSymbolParams()));
+
+    JsonObject settings = new JsonObject();
+    settings.add("ltex", new JsonObject());
+    Assertions.assertDoesNotThrow(() -> service.didChangeConfiguration(
+        new DidChangeConfigurationParams(settings)));
+
+    Assertions.assertDoesNotThrow(() -> service.didChangeWatchedFiles(
+        new DidChangeWatchedFilesParams()));
+
+    ExecuteCommandParams params = new ExecuteCommandParams("ltex.foobar", Collections.emptyList());
+    JsonObject result = ((JsonElement)service.executeCommand(params).get()).getAsJsonObject();
+    Assertions.assertFalse(result.get("success").getAsBoolean());
+  }
+
+  @Test
+  public void testCheckDocument() throws IOException, InterruptedException, ExecutionException,
         URISyntaxException {
     assertCheckDocumentResult("invalid_uri", false);
     assertCheckDocumentResult("file:///non_existent_path", false);
-    File tmpFile = File.createTempFile("ltex-", ".tex");
 
-    try {
-      assertCheckDocumentResult(tmpFile.toURI().toString(), true);
-    } finally {
-      if (!tmpFile.delete()) {
-        Tools.logger.warning(Tools.i18n(
-            "couldNotDeleteTemporaryFile", tmpFile.toPath().toString()));
+    for (String extension : Arrays.asList(".bib", ".md", ".tex")) {
+      File tmpFile = File.createTempFile("ltex-", extension);
+
+      try {
+        assertCheckDocumentResult(tmpFile.toURI().toString(), true);
+      } finally {
+        if (!tmpFile.delete()) {
+          Tools.logger.warning(Tools.i18n(
+              "couldNotDeleteTemporaryFile", tmpFile.toPath().toString()));
+        }
       }
     }
+  }
+
+  @Test
+  public void testGetServerStatus() throws InterruptedException, ExecutionException {
+    LtexLanguageServer server = new LtexLanguageServer();
+    LtexWorkspaceService service = new LtexWorkspaceService(server);
+    ExecuteCommandParams params = new ExecuteCommandParams("ltex.getServerStatus",
+        Collections.emptyList());
+    JsonObject result = ((JsonElement)service.executeCommand(params).get()).getAsJsonObject();
+
+    Assertions.assertTrue(result.get("success").getAsBoolean());
+    Assertions.assertTrue(result.get("processId").getAsLong() >= 0);
+    Assertions.assertTrue(result.get("wallClockDuration").getAsDouble() >= 0);
+
+    if (result.has("cpuUsage")) {
+      Assertions.assertTrue(result.get("cpuUsage").getAsDouble() >= 0);
+    }
+
+    if (result.has("cpuDuration")) {
+      Assertions.assertTrue(result.get("cpuDuration").getAsDouble() >= 0);
+    }
+
+    Assertions.assertTrue(result.get("usedMemory").getAsDouble() >= 0);
+    Assertions.assertTrue(result.get("totalMemory").getAsDouble() >= 0);
   }
 }
